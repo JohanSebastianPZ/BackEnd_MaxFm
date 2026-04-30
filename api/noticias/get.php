89d6da0d109178d57c12acc46c076d57bab8c009
JohanSebastianPZ <johansebastian627@gmail.com>
2026-05-01 00:07:10 +0200

    Funcion mostrar informacion tabla noticias

---
+++ api/noticias/get.php
@@ -0,0 +1,21 @@
+<?php
+require_once "../../config/cors.php";
+require_once "../../config/database.php";
+
+configurarCORS();
+$db = conectarDB();
+
+try {
+    // Obtenemos los locutores activos ordenados por el campo 'orden'
+    $stmt = $db->prepare("SELECT * FROM noticias");
+    $stmt->execute();
+    $locutores = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    echo json_encode([
+        "success" => true,
+        "data" => $locutores
+    ]);
+
+} catch (PDOException $e) {
+    echo json_encode(["success" => false, "message" => $e->getMessage()]);
+}
